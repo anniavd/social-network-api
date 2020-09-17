648231e5d502e4ae50ceca6bbbad6e6db3b14c4a
annia valdes <millerl.mb79@gmail.com>
2020-09-16 22:18:36 -0500

    create a Thought model

---
+++ models/Thought.js
@@ -0,0 +1,74 @@
+const { Schema, model, Types } = require('mongoose'); //Import from the library 
+const moment = require('moment');
+
+
+const reactionSchema = new Schema(  
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
+
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true,
+        },      
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total reactions of thought 
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+
+
+// create Thought model using the ThoughtSchema
+const Thought = model('Thought', ThoughtSchema);
+
+// export the Pizza model
+module.exports = Thought;
