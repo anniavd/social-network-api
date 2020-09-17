a079e896ea4cab63df7b320da0db9014ef6ff5a0
annia valdes <millerl.mb79@gmail.com>
2020-09-17 17:25:28 -0500

    add delete user by id

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -57,11 +57,34 @@ createUser({ body }, res) {
       .catch(err => res.json(err));
   },
 
-  deleteUser({ body }, res) {
-    
+  //delete user by id
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
   },
-  addFriend({ body }, res) {
-    
+
+  addFriend(req, res) {
+   /* User.findOneAndUpdate(
+      {_id:req.params.userId},
+      {$addToSet:{friends:{friendsId:req.params.friendId}}}
+    )
+    .then(dbUserData => {
+      if (!dbUserData) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });*/
   },
 
   deleteFriend({ body }, res) {
