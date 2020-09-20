1f92b10770034f8c679240ff1015643772523e1d
annia valdes <millerl.mb79@gmail.com>
2020-09-20 13:23:44 -0500

    add a message when show user by id no is found

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -37,11 +37,14 @@ createUser({ body }, res) {
           select:'-__v'
       })
       .select('-__v')
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.sendStatus(400);
-      });
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
   },
 
   //update user info
